6882a9ea36ae8fa6d538e10ca2eb2b9a5f700a5e
NipSauce <SvenParker@wlanguest140208.eduroam.hj.se>
2015-02-04 12:09:28 +0100

    Additional fixes
    randomizer fixed
    totalScore & roundScore partially fixed
    html & css updated
    resetScore & errorMessage added

--- js/js.js
+++ js/js.js
@@ -9,46 +9,52 @@
 function randomizer (bottom, top) {
 	return Math.floor( Math.random() * ( 1 + top - bottom ) ) + bottom;
 }
-
-var randomizer = randomizer (1, 6);
-
+var roundScore = 0;
 var chancesLeft = 10;
 
-if (chancesLeft>0){
-	function diceRoll (d1,d2,d3,d4,chancesLeft){
-		for (; chancesLeft>=0 ; chancesLeft--) {
-			var d1 = Math.floor( Math.random() * ( 1 + 6 - 1 ) ) + 1;
-			var d2 = Math.floor( Math.random() * ( 1 + 6 - 1 ) ) + 1;
-			var d3 = Math.floor( Math.random() * ( 1 + 6 - 1 ) ) + 1;
-			var d4 = Math.floor( Math.random() * ( 1 + 6 - 1 ) ) + 1;
-			guess = new Number(document.getElementById('guess').value);
-			document.getElementById('d1').innerHTML = d1;
-			document.getElementById('d2').innerHTML = d2;
-			document.getElementById('d3').innerHTML = d3;
-			document.getElementById('d4').innerHTML = d4;
-			var diceSum = (d1+d2+d3);
-			console.log("The 3 dice sum is: "+diceSum);
-			console.log("The 4th die shows: "+d4);
-			document.getElementById('diceSum').innerHTML = diceSum;
-			if (guess==diceSum){
-				var roundScore = diceSum*d4;
-				document.getElementById('roundScore').innerHTML = roundScore;
-				console.log("Your score this round is: "+roundScore);
-				console.log("Correct guess! "+guess+" was right!");
-			}else {
-				var roundScore = 0;
-				document.getElementById('roundScore').innerHTML = roundScore;
-				console.log("Incorrect guess, try again.");
-			}
-			document.getElementById('chancesLeft').innerHTML = chancesLeft;
-			console.log("You have "+chancesLeft+" chances left");
+function diceRoll (d1,d2,d3,d4){
+	var totalScore =+ roundScore;
+	if (chancesLeft>0){
+		var d1 = randomizer (1, 2);
+		var d2 = randomizer (1, 2);
+		var d3 = randomizer (1, 2);
+		var d4 = randomizer (1, 2);
+		guess = new Number(document.getElementById('guess').value);
+		document.getElementById('d1').innerHTML = d1;
+		document.getElementById('d2').innerHTML = d2;
+		document.getElementById('d3').innerHTML = d3;
+		document.getElementById('d4').innerHTML = d4;
+		
+		var diceSum = (d1+d2+d3);
+		console.log("The 3 dice sum is: "+diceSum);
+		console.log("The 4th die shows: "+d4);
+		document.getElementById('diceSum').innerHTML = diceSum;
+		
+		if (guess==diceSum){
+			roundScore = diceSum*d4;
+			document.getElementById('roundScore').innerHTML = roundScore;
+			document.getElementById('totalScore').innerHTML = totalScore;
+			console.log("Your score this round is: "+roundScore);
+			console.log("Correct guess! "+guess+" was right!");
+		}else {
+			document.getElementById('roundScore').innerHTML = roundScore;
+			document.getElementById('totalScore').innerHTML = totalScore;
+			console.log("Incorrect guess, try again.");
 		}
+		chancesLeft--;
+		document.getElementById('chancesLeft').innerHTML = chancesLeft;
+		console.log("You have "+chancesLeft+" chances left");
+	}else{
+		errorMessage ("Try Again pleeez!");
 	}
-}else {
-	console.log("Try Again!");
 }
-/*function diceRoll (d1, d2, d3){
-	for (var i=0; i<dice.length-1; i++) {
-	    console.log(Hej);
-	}
-}*/
+
+function errorMessage (error){
+	document.getElementById('errorDisplay').innerHTML = error;
+	console.log(error);
+}
+
+function resetScore (){
+	totalScore = 0;
+	roundScore = 0;
+}
